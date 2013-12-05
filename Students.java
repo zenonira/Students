26ec2c87bbb1481031ab90976de3fe2c706dcbf3
apcs <apcs@mvhs-u528ap03.dcsdk12.org>
2013-12-05 09:30:27 -0700

    Working

--- Students.java
+++ Students.java
@@ -13,29 +13,46 @@ public class Students {
 		Scanner scan = new Scanner(file);
 		String[] students = scan.nextLine().split(",");
 		String[] called;
+		int person = 0;
 		// System.out.println("" + students[0]);
-		Random r = new Random();
-		int person = r.nextInt(students.length);
 		if (i == 1) {
+			Random r = new Random();
+			person = r.nextInt(students.length);
 			System.out.println(students[person]);
 		} else if (i == 2) {
 			System.out.println("How big of groups?");
 			int x = start.nextInt();
+			shuffle(students);
 			for (int b=0; b<students.length-1; b++) {
 				for (int a=0; a<x; a++) {
-					person = r.nextInt(students.length);
 					System.out.print(students[person] + " ");
-					called[b] = students[person];
+					//called[b] = students[person];
 					b++;
+					person++;
 				}
 				b--;
-				System.out.println();
+				System.out.println();	
+			}
+			if (students.length % x > 0) {
+				for (int j = students.length; j % x >= 1; j--) {
+					System.out.println("Left over " + students[j-1]);
+				}
 			}
 			
 		} else {
 			System.out.println("Not a number");
 		}
 		
+	}
+
+	public static void shuffle(String[] students) {
+		Random r = new Random();
+		for (int f = (students.length - 1); f > 0; f--) {
+		    String name = students[f];
+		    int temp = r.nextInt(f);
+		    students[f] = students[temp];
+		    students[temp] = name;
+		}
 	}
 
 
